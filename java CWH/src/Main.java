541f13c441439f902b585da879cd4fc224f9aa50
aaryan <aaryan.chauhan.1007@gmail.com>
2023-09-19 21:44:10 +0530

    java first project

---
+++ java CWH/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("The sum of 3 numbers");
+        int num1 = 5;
+        int num2 = 6;
+        int num3 = 7;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+        System.out.println("pim");
+        System.out.println(num1 + num2 + num3);
+    }
+}
