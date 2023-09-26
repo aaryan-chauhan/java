728b99139def366edd6bdcf1c390b9ca84d872bc
aaryan <aaryan.chauhan.1007@gmail.com>
2023-09-26 23:56:21 +0530

    the java programme doesnt run

--- CWH_practice_set.java
+++ CWH_practice_set.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class CWH_practice_set {
-    public static void main(String[] args) {
+
+    //question2
+    float subject1 = 39;
+    float subject2 = 49;
+    float subject3 = 59;
+    float cgpa = (num1 + num2 + num3)/30;
+
+    //question3public static void main(String[] args) {
         //question1
         int num1 = 3;
         int num2 = 4;
         int num3 = 5;
         int sum = num1 + num2 + num3;
 
-        //question2
-        float subject1 = 39;
-        float subject2 = 49;
-        float subject3 = 59;
-        float cgpa = (num1 + num2 + num3)/30;
-
-        //question3
         Scanner string = new Scanner(system.in);
         System.out.println("please enter your name");
                 String name = string.next();
